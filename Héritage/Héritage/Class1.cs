a5aa2546b1c367e28a0e523a1699b3f7e3909901
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-24 10:39:18 +0200

    Add files via upload

---
+++ Héritage/Héritage/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Héritage
+{
+    public abstract class Animal
+    {
+        protected string Name { get; }
+        protected int LegsCount { get; }
+        protected bool Hairy { get; }
+
+        public Animal(string name, int legsCount, bool hairy)
+        {
+            this.Name = name;
+            this.LegsCount = legsCount;
+            this.Hairy = hairy;
+        }
+
+        public virtual string Move()
+        {
+            return $"Le/l'{this.Name} se déplace à {LegsCount} pattes"; 
+        }
+
+        public string Eat()
+        {
+            return "L'animal mange avec la bouche";
+        }
+    }
+
+
+    public class Bipede : Animal
+    {
+        public Bipede(string name) : base(name, 2, true)
+        { }
+
+        public override string Move()
+        {
+            return base.Move();
+        }
+    }
+
+    public class Quadrupede : Animal
+    {
+        public Quadrupede(string name) : base(name, 4, false)
+        { }
+
+        public override string Move()
+        {
+            return base.Move();
+        }
+
+    }
+
+}
---
+++ Héritage/Héritage/Program.cs
@@ -0,0 +1,14 @@
+﻿using Héritage;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Bipede a1 = new Bipede("Singe");
+
+        Quadrupede q1 = new Quadrupede("Elephant");
+
+        Console.WriteLine(a1.Move());
+        Console.WriteLine(q1.Move());
+    }
+}
